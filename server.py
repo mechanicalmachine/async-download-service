1022ad92e3463c0924a5dc284783951098a46832
anatoly <anatolijplotnikov1@gmail.com>
2021-04-27 00:05:13 +0700

    init archiving view

--- server.py
+++ server.py
@@ -2,8 +2,34 @@ from aiohttp import web
 import aiofiles
 
 
-async def archivate(request):
-    raise NotImplementedError
+
+async def archivate(request: BaseRequest) -> StreamResponse:
+    response = web.StreamResponse()
+
+    response.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
+
+    await response.prepare(request)
+
+    archive_hash = request.match_info.get('archive_hash')
+    archiving_dir_path = Path('test_photos', archive_hash)
+    # archiving_dir_path = 'test_photos'
+
+    archiving = await asyncio.create_subprocess_exec(
+        'zip',
+        '-r',  # recursively
+        '--base_dir cwd',
+        '-',  # send result to stdout
+        archiving_dir_path,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    chunk_size_in_bytes = 200 * 2024
+
+    while not archiving.stdout.at_eof():
+        await response.write(await archiving.stdout.read(n=chunk_size_in_bytes))
+
+    return response
 
 
 async def handle_index_page(request):
